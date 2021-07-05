a114cb7aca0b68cc1b1801bc5594d9e4ad8ba06c
Carlos Magno <32673318+carlosmagno02@users.noreply.github.com>
2021-07-05 09:47:12 -0300

    responsivo

--- js/menu.js
+++ js/menu.js
@@ -7,4 +7,19 @@ $(function(){
 
         return false
     })
+
+    $('nav.mobile').on('click', function(){
+        var listaMenu = $('nav.mobile ul')
+
+        if(listaMenu.is(":hidden") == true){
+            var botao =  document.getElementsByClassName("botao-menu-mobile")
+            listaMenu.slideToggle()
+        }else{
+            
+            var icone = $('.botao-menu-mobile i')
+            icone.addClass('fa-bars')
+            icone.removeClass('fa-times')
+            listaMenu.slideToggle()
+        }
+    })
 })
